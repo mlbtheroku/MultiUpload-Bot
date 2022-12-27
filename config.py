f7d4408b826a8b750c6fdd2f8cabab2b42129b2e
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2022-12-27 19:46:12 +0630

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
    API_ID= int(os.environ.get("API_ID", 17894641))
    API_HASH = os.environ.get("API_HASH", "4e5b39e5c7c6066e5144dfc50cf466cf")
    # get a token from @BotFather
-   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+   BOT_TOKEN = os.environ.get("BOT_TOKEN", "5823556553:AAHQgarPyBdfxpc9UcEuorb49dGDM0m_b4c")
    # channel id = -100 (for logs)
    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001705637043"))   
    # Array to store users who are authorized to use the bot 
